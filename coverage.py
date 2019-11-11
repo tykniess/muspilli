afc94a8a0e723bdcd36972586472429882a500a7
Tyler Kniess <tyler@Tylers-MacBook-Pro.local>
2019-11-10 23:52:20 -0500

    add masculine a-stem nouns

---
+++ coverage.py
@@ -0,0 +1,38 @@
+import csv
+import sys
+import random
+
+heliand_words  = []
+muspilli_words = []
+covered_words  = []
+
+heliand = 'heliand/hel-tokens.txt'
+muspilli= 'forms.txt'
+
+with open(muspilli, 'r', newline='\n') as mfile:
+    reader=csv.reader(mfile, delimiter='\t')
+    for row in reader:
+        muspilli_words.append(row[0].strip())
+
+with open(heliand, 'r', newline='\n') as hfile:
+    for row in hfile:
+        heliand_words.append(row.strip())
+
+for token in heliand_words:
+    if token.lower() in muspilli_words:
+        covered_words.append(token)
+
+print("\nThere are "+str(len(heliand_words))+" unique tokens in the Hêliand.")
+print("Project Muspilli currently contains "+str(len(muspilli_words))+" synthetically-generated forms.")
+print("Of the "+str(len(heliand_words))+" unique tokens in the Hêliand, Project Muspilli contains " +\
+      str(len(covered_words)) + " forms, or "+str(100*len(covered_words)/len(heliand_words))+"%. Wow!\n") 
+
+with open('README.md', 'w') as README:
+	README.write("")
+
+with open('README.md', 'a') as README:
+        README.write("""Introducing Muspilli, a Finite-State Transducer for the Old Saxon language.\nBuilt on the framework of Helsinki Finite-State Technologies (HFST) using Köbler's Old Saxon Dictionary.\nCurrent output forms are in forms.txt\n\n
+""")
+
+with open('README.md', 'a') as README:
+	README.write("There are "+str(len(heliand_words))+" unique tokens in the Hêliand.\nProject Muspilli currently contains "+str(len(muspilli_words))+" synthetically-generated forms.\nOf the "+str(len(heliand_words))+" unique tokens in the Hêliand, Project Muspilli contains " + str(len(covered_words)) + " forms, or "+str(100*len(covered_words)/len(heliand_words))+"%. Wow!\n")
--- dictionaries/scrape_nouns.py
+++ dictionaries/scrape_nouns.py
@@ -7,7 +7,7 @@ lemmas = []
 lemmas_cleaned = []
 nums = ['1','2','3','4','5','6','7','8','9','0']
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','k','l','m','n','o',
-            'p','q','r','s','t','u','v','w','x','y','z',
+            'p','q','r','s','t','u','v','w','x','y','z', 'đ', 'ƀ',
             'ā','â','ā',
             'ê','ė', 'ē',
             'ô','ō',
@@ -19,30 +19,38 @@ POS_tags = ['as., st. V. (1)', 'as., st. V. (2)', 'as., st. V. (3a)', 'as., st. 
 'as., red. V.',\
             'as., st. M. (a)']
 
-def dict_scrape(POS, dictionaryfile='as_freq.txt'):
+def dict_scrape(POS, dictionaryfile='as_kurzform.txt'):
     """Scrapes a dictionary for a given part of speech. POS tags in POS_tags.
 
     POS(str), dictionaryfile(str-of-filename) -> list-of-strings
     """
     if POS in POS_tags:
-        with open(dictionaryfile) as to_scrape:
+        with open(dictionaryfile, 'r') as to_scrape:
             for line in to_scrape:
                 if POS in line:
                     lemmas.append(line)
+    #cleaning
     for line in lemmas:
-#1,                                       bāga, as., st. F. (ō)?, sw. F. (n)?: nhd. Streit
         i=0
-        for char in line[:44]:
-            if char not in alphabet:
+        for char in line[:20]:
+            if char == "*" or char == " ":
+                break
+            else:
                 i=i+1
-        lemmas_cleaned.append(line[i:].strip().replace('*','').replace('?','')+'\n')
+        lemmas_cleaned.append(line[:i].lower().strip().replace('*','').replace('?','')+'\n')
         #scrub line of the frequency data, begin with headword?
 
 ##    print("Found " + str(len(lemmas_cleaned)) + " lemmas matching that category")
     return lemmas_cleaned   
 
-
 dict_scrape(sys.argv[1])
-for line in lemmas_cleaned:
-    with open(sys.argv[1][5:],'w+') as to_write:
+with open(sys.argv[1][5:],'w+') as to_write:
+    for line in lemmas_cleaned:
         to_write.write(line)
+
+
+#encode masculine a-stem nouns:
+with open(sys.argv[1][5:]+'_ready-for-import','w+') as to_write:
+    for line in lemmas_cleaned:
+        to_write.write(line[:-1]+"%<noun%>%<masc%>%<a-stem%>:"+line[:-1]+ " Masc-A-Stem ;\n"
+                       )
