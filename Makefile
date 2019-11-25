9e811b1ff23fcb892aeb34e8f7fd94ecc7331ef3
Tyler Kniess <tyler@Tylers-MacBook-Pro.local>
2019-11-25 18:15:14 -0500

    change hel.txt and clean.txt "b/v" to "ƀ" and "d" to "đ" in contexts where this contrasts may previously have been levelled."

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all:
 	cat os.lexc > os-copy.lexc
-	cat os-copy.lexc | gsed s/'ë'/'e'/g | gsed s/'ī'/'î'/g |gsed s/'ē'/'ê'/g | gsed s/'ō'/'ô'/g | gsed s/'ā'/'â'/g | gsed s/'ė'/'e'/g | gsed s/'ū'/'û'/g > os.lexc
+	cat os-copy.lexc |gsed s/'v'/'ƀ'/g | gsed s/'ë'/'e'/g | gsed s/'ī'/'î'/g |gsed s/'ē'/'ê'/g | gsed s/'ō'/'ô'/g | gsed s/'ā'/'â'/g | gsed s/'ė'/'e'/g | gsed s/'ū'/'û'/g > os.lexc
 	hfst-lexc -v os.lexc -o os.lexc.hfst
 	hfst-twolc os.twol -o os.twol.hfst
 	hfst-compose-intersect -v -o os.gen.hfst os.lexc.hfst os.twol.hfst
--- heliand/html/proc.py
+++ heliand/html/proc.py
@@ -5,16 +5,27 @@ import re
 
 soup = BeautifulSoup(open(sys.argv[1]), 'html.parser')
 
-doc=soup.find('div', {'class':'contentus'})
+#doc=soup.find('div', {'class':'contentus'})
 
-text=doc.get_text()
+#text=doc.get_text()
+
+text=soup.get_text()
 
 text = re.sub('[0-9]+', '\1 ',text)
 
-for c in '{[|]}':
+for c in '[|]':
 	text=text.replace(c,'')
 
+text=text.replace('{b}','ƀ').replace('{d}','đ').replace('. . . . . . . . . . . . . . .  . . . . . . . . . . . . . . . ','').replace('Heliand','').replace('Fitte','').replace('______________________________________________________________________________','').replace('Die Evangelisten (Book of Kells, um  )','').replace('______________________________________________________________________________','').replace('<<< Übersicht  <<< vorige Seite','').replace('BIBLIOTHECA AUGUSTANA','').replace('nächste Seite >>>','').replace('bibliotheca Augustana','').replace('\num\n','').strip()
+#text=" ".join(text.split())     #this optional step removes a lot of whitespace
 print(text)
+
+
+
+#call this on hel.txt
+
+#####
+
 
 #extra_symbols = ['(',')','[',']','{','}','{','}','1','2','3','4','5','6','7','8','9','10']
 
